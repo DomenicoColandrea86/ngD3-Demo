778da036adb343a83d1f926569ee6adc6c14133a
Domenico Colandrea <me@domenicocolandrea.com>
2013-10-04 18:56:39 -0400

    updated some styling and other updates

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
   app = express();
 
   // for development
-  //app.use(express['static'](__dirname + '/app'));
-  app.use(express['static'](__dirname + '/dist'));
+  app.use(express['static'](__dirname + '/app'));
+  //app.use(express['static'](__dirname + '/dist'));
 
   app.listen('8080');
   console.log('Server started at http://localhost:8080');
